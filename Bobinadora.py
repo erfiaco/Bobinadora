803aefc8b74a46b6d0be704cff24bac5f4231c54
Javier Rojas <erfiaco@gmail.com>
2025-03-10 13:31:28 +0100

    Correcciones de deepseek

--- Bobinadora.py
+++ Bobinadora.py
@@ -5,16 +5,16 @@ step_pin = 17
 dir_pin = 18
 
 #creo instancia del motor
-Stepper = RpiMotorLib.A4988Nema(dir_pin, step_pin,(5,6,13), "DRV8825")
+stepper = RpiMotorLib.A4988Nema(dir_pin, step_pin,(5,6,13), "DRV8825")
 
 #configuración del motor
-speed = 4000 (pasos por segundo)
+speed = 4000 #pasos por segundo
 steps_per_revolution = 800 #200 pasos x 4 (microstepping)
 
 try:
   print('Moviendo motor a vel constante, ctrl + c para detener el motor')
   while True:
-    stepper.motor_go(True, "Full", steps_per_revolution, 1/speed, True, 0)
+    stepper.motor_go(True, "Full", steps_per_revolution, 1.0 / speed, True, 0)
 
 except KeyboardInterrupt:
   print('Programa detenido por el usuario')
