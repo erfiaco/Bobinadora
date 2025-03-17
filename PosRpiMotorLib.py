be5206fb538748cc8b35289a3ae7bf4ec62e1e98
Javier Rojas <erfiaco@gmail.com>
2025-03-17 01:17:59 +0100

    Update PosRpiMotorLib.py

--- PosRpiMotorLib.py
+++ PosRpiMotorLib.py
@@ -21,6 +21,26 @@ running = True
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button with pull-up resistor
 
+def generate_steps_matrix(self, positions):
+        """
+        Genera una matriz con el número de pasos necesarios y la dirección para alcanzar
+        cada posición objetivo desde la posición actual.
+    
+        :param positions: Lista de posiciones en el eje X.
+        :return: Lista de listas (matriz) con pasos y direcciones.
+        """
+        current_position = 0
+        steps_matrix = []
+
+        for target_position in positions:
+            steps = abs(target_position - current_position)
+            direction = True if target_position > current_position else False
+            steps_matrix.append([steps, direction])
+            current_position = target_position  # Actualiza la posición actual
+    
+        return steps_matrix
+
+
 Posiciones = [400,20,1,5,6,8]
 
 
@@ -37,7 +57,7 @@ try:
         speed = map_value(pot_value, 0, 32767, min_speed, max_speed)
   
         #mover motor a posicion determinada
-        stepper.motor_go(True, "Full", steps_per_revolution, 0.001, False, 1/speed)
+        stepper.motor_go(steps_matrix[, "Full", steps_per_revolution, 0.001, False, 1/speed)
   
         i += 1
           
