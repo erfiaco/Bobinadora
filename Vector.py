fea3041d8c294240f63a468d7f88c171af9e79f5
Javier Rojas <erfiaco@gmail.com>
2025-04-14 01:15:26 +0200

    Update Vector.py

--- Vector.py
+++ Vector.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-vector = np.arange(5000)
+# Generamos el vector de 5000 posiciones aleatorias entre 0 y 400 (inclusive)
+vector = np.random.randint(0, 401, size=5000)
+
+Test = [400, 300, 400, 300, 400, 300]
 
 np.save("vector_guardado.npy", vector)
+np.save("Test.npy", Test)
